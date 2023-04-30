b00dd0267b49aafdb0714e034e5309bbb67c4e6b
RamonRossello <128502837+RamonRossello@users.noreply.github.com>
2023-04-30 23:21:18 +0200

    3 files modified

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
+
+
+d3.json("https://raw.githubusercontent.com/RamonRossello/08MBID---Visualizacion-de-Datos/RamonRossello-text/accidentes_gravedad.json", function(data) {
+var visualization = d3plus.viz()
+.container("#viz1")
+.data(data)
+.type("box")
+.id("ID")
+.x("Gravedad")
+.y("Tiempo medio")
+.draw()
+});
